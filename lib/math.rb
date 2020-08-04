af93253b00bd5c742cfe88bb59c040bc8ba265ac
voracious-accelerator-8615 <margo.reiss.gold@gmail.com>
2020-08-04 00:18:18 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,15 +5,21 @@ add
 end
 
 def subtraction(num1, num2)
-
+subt = num1 - num2
+puts subt
+subt
 end
 
 def division(num1, num2)
-
+div = num1 / num2
+puts div
+div
 end
 
 def multiplication(num1, num2)
-
+mul = num1 * num2
+puts mul
+mul
 end
 
 def modulo(num1, num2)
